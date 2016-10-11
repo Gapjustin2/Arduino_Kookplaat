fee851b3af5d4af1aca98a5e73484806c25c93f8
Elwin132 <enduinkerken@gmail.com>
2016-10-11 12:43:45 +0200

    Buttons

--- Code.C
+++ Code.C
@@ -1,14 +1,9 @@
 int ledPins[] = {11,10,A5,A4,A3,A2,A1,A0};
 int delayvar[] = {1,20,40,80,150,250,1000};
-int ledState[] = {0, 0, 0, 0, 0, 0, 0, 0};
 int shiftOutNumbers[] = {320, 377, 292, 304, 281, 274, 258};
 int shiftOutVar[] = {128, 384, 896, 1920, 3968, 8064, 16256, 32640};
 int shiftOutArrayVar[] = {448, 704, 1216, 2240, 4288, 8384, 16576, 32960};
 int factor = 255 / 6;
-int buttonPins[] = {2, 4, 7, 9, 12};
-int buttonPressed[] = {0, 0, 0, 0, 0};
-int ledPinsSize = sizeof(ledPins) / sizeof(int);
-int buttonPinsSize = sizeof(buttonPins) / sizeof(int);
 int selection = 0;
 int latchPin = 5; //pin 12 of 595
 int dataPin = 3; //pin 14 on the 595
@@ -19,13 +14,24 @@ boolean on1 = true;
 boolean safetyOff0 = true;
 boolean safetyOff1 = true;
 
+int outputPins[] = {2, 5, 4, 6};
+int outputPinSize = sizeof(outputPins) / sizeof(int);
+int currentOutputPin = 0;
+
+int inputPins[] = {7, 8};
+int inputPinSize = sizeof(inputPins) / sizeof(int);
+
+int buttonsPressed[] = {0, 0, 0, 0, 0, 0, 0, 0};
+int buttonSize = sizeof(buttonsPressed) / sizeof(int);
+
+int place;
 
 void setup() {
-  for (int i = 0; i < ledPinsSize; i++) {
-    pinMode(ledPins[i], OUTPUT);
+  for (int i = 0; i < outputPinSize; i++) {
+    pinMode(outputPins[i], OUTPUT);
   }
-  for (int i = 0; i < buttonPinsSize; i++) {
-    pinMode(buttonPins[i], INPUT_PULLUP);
+  for (int i = 0; i < inputPinSize; i++) {
+    pinMode(inputPins[i], INPUT_PULLUP);
   }
   Serial.begin(9600);
   pinMode(latchPin, OUTPUT);
@@ -34,25 +40,49 @@ void setup() {
 }
 
 void displayLEDS() {
-for(int i=0; i<ledPinsSize;i++){
-digitalWrite(ledPins[i],HIGH);
-delayMicroseconds(delayvar[ledState[i]]);
-digitalWrite(ledPins[i],LOW);
+  for(int i = 0; i < ledPinsSize; i++) {
+    digitalWrite(ledPins[i], HIGH);
+    delayMicroseconds(delayvar[ledState[i]]);
+    digitalWrite(ledPins[i], LOW);
+  }
 }
+
+void checkButtonsPressed() {
+  for (int i = 0; i < inputPinSize; i++) {
+    int buttonState = digitalRead(inputPins[i]);
+    place = currentOutputPin*inputPinSize + i;
+    if (buttonState == LOW) {
+      buttonsPressed[place] = 1;
+    } else {
+      if buttonsPressed[place] == 1 {
+        clickButton(place);
+      }
+      buttonsPressed[place] = 0;
+    }
+  }
+}
+
+void nextOutputPin() {
+  currentOutputPin++;
+  if (currentOutputPin >= outputPinSize) {
+    currentOutputPin = 0;
+  }
+}
+
+void setOutputPins() {
+  for (int i = 0; i < outputPinSize; i++) {
+    if (i == currentOutputPin) {
+      digitalWrite(outputPins[i], LOW);
+    } else {
+      digitalWrite(outputPins[i], HIGH);
+    }
+  }
 }
 
 void checkButtons() {
-  for (int i = 0; i < buttonPinsSize; i++) {
-    int buttonState = digitalRead(buttonPins[i]);
-    if (buttonState == LOW) {
-      buttonPressed[i] = 1;
-    } else {
-      if (buttonPressed[i] == 1) {
-        clickButton(i);
-      }
-      buttonPressed[i] = 0;
-    }
-  }
+  checkButtonsPressed();
+  nextOutputPin();
+  setOutputPins();
 }
 
 void clickButton(int x) {
@@ -166,7 +196,6 @@ void toggleSafetySwitch(){
     } else {
         safetyOff1 = !safetyOff1;
     }
-
 }
 
 void shiftOutNumber(int number){
@@ -183,10 +212,9 @@ void display(int displaynumber){
 void loop() {
   checkButtons();
   displayLEDS();
-for(int i = 0; i < ledPinsSize; i++){
-  shiftOutNumber(shiftOutArrayVar[i]);
-  delay(1);
-}
-
+  for(int i = 0; i < ledPinsSize; i++){
+    shiftOutNumber(shiftOutArrayVar[i]);
+    delay(1);
+  }
 }
  
